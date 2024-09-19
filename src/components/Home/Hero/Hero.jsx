979f38bb4d9c7e3b93f88d937fc81f60464724d5
Kasinathan22 <89632353+Kasinathan22@users.noreply.github.com>
2024-09-19 17:55:12 +0530

    Update Hero.jsx

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -71,7 +71,8 @@ const Hero = () => {
       <div className='pt-24 md:lg:pt-36 '>
         <div className='h-12 w-full bg-gre flex justify-center items-center'>
           <h1 className='text-black font-bold text-sm md:text-xl lg:text-xl text-center'>
-            India's Largest Refurbished Mobile Phone Store
+          India&apos;s Largest Refurbished Mobile Phone Store
+
           </h1>
         </div>
       </div>
