73b633d6b061758f3e36f490ad7e1719b11076a7
Kasinathan22 <89632353+Kasinathan22@users.noreply.github.com>
2024-09-30 21:27:38 +0530

    Update Into.jsx

--- src/components/Home/Into/Into.jsx
+++ src/components/Home/Into/Into.jsx
@@ -1,17 +1,24 @@
 "use client"
 import React from 'react';
+import P from "../../../../public/image/SalesPhone/i.png"
+import M from "../../../../public/image/SalesPhone/m.png"
+import W from "../../../../public/image/SalesPhone/w.png"
+import T from "../../../../public/image/SalesPhone/tv.png"
+import S from "../../../../public/image/SalesPhone/s.png"
+import H from "../../../../public/image/SalesPhone/h.png"
+
 
 function Into() {
   // Sample JSON data for the products
   const products = [
-    { id: 1, name: "Laptop", image: "/image/Salesphone/i.png" },
-    { id: 2, name: "Moblie", image: "/image/Salesphone/m.png" },
-    { id: 3, name: "Smart watches", image: "/image/Salesphone/w.png" },
-    { id: 4, name: "Product 4", image: "/image/Salesphone/tv.png" },
-    { id: 5, name: "Product 5", image: "/image/Salesphone/s.png" },
-    { id: 6, name: "Product 6", image: "/image/Salesphone/h.png" },
-    { id: 7, name: "Product 7", image: "/image/Salesphone/i.png" },
-    { id: 8, name: "Product 8", image: "/image/Salesphone/i.png" },
+    { id: 1, name: "Laptop", image: P},
+    { id: 2, name: "Moblie", image: M },
+    { id: 3, name: "Smart watches", image: W },
+    { id: 4, name: "Product 4", image: T},
+    { id: 5, name: "Product 5", image: S },
+    { id: 6, name: "Product 6", image: H},
+    { id: 7, name: "Product 7", image: P },
+    { id: 8, name: "Product 8", image: M },
 
 
   ];
